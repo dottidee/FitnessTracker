3406d86f8501b061cce116eee4a41d185ea62e8f
Dorothy <dorothyscheines@gmail.com>
2019-11-07 14:29:47 -0500

    ui?

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require ('express');
-const bodyParser = require ('body-parser');
+//const bodyParser = require ('body-parser');
 const cors = require ('cors');
-const expressLayouts = require('express-ejs-layouts');
+//const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-//Middleware
-app.use(cors());
+    //Middleware
+    app.use(cors());
    
     //routes
     app.use('/', require('./routes/users'))
@@ -17,13 +18,14 @@ app.use(cors());
     const db = require('./config/keys').MongoURI;
 
     //connect to mongo
-    mongoose.connect(db, { useNewUrlParser: true})
+    mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true})
     .then(() => console.log('MongoDB COnnected...'))
     .catch(err => console.log(err));
 
-    //ejs
+    /*/ejs
     app.use(expressLayouts);
     app.set('view engine', 'ejs');
+    */
 
     //Bodypa
     app.use(express.urlencoded({ extended: false }));
@@ -33,6 +35,5 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, (err) => console.log(`Server started on port ${port}`));
